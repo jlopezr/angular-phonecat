1eb05fab35f26eae8bbe8fd1ca9a6cc94de402b8
Juan López Rubio <j.lopez.r@gmail.com>
2014-12-03 18:30:21 +0100

    +++ / works

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+// Home returns the index
+app.get('/', function(req, res, next) {
+        res.sendFile(path.join(__dirname, 'app', 'index.html'));
+});
+
+// All /app routes return the index.
 app.get('/app/*', function(req, res,next) {
     if(req.url.indexOf('.') == -1) {
         res.sendFile(path.join(__dirname, 'app', 'index.html'));
@@ -10,6 +16,7 @@ app.get('/app/*', function(req, res,next) {
     }
 });
 
+// The default case gets the static files
 app.use(express.static(__dirname));
 
 app.listen(3000);
